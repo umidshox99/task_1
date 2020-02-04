ab01dad9d4e6b6a28c8a8d9e68e561656a1a60f8
UmidShox <umidjonshoniyozov@gmail.com>
2020-02-04 19:05:53 +0500

    clean all of initial code

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,20 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:task_1/presentation/screens/home.dart';
+import 'package:task_1/presentation/screens/sign_in.dart';
 import 'package:task_1/presentation/screens/sign_up.dart';
+import 'package:task_1/presentation/screens/slpash_screen.dart';
+import 'package:task_1/presentation/screens/verify_page.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            primaryColor: Colors.red, accentColor: Colors.yellowAccent),
-        home: SplashPage());
-  }
-}
-
-class SplashPage extends StatefulWidget {
-  @override
-  _SplashPageState createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-        Duration(seconds: 2),
-        () => {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext context) => SignUp()))
-            });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(color: Colors.redAccent),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 50.0,
-                      child: Icon(
-                        Icons.add_shopping_cart,
-                        color: Colors.greenAccent,
-                        size: 50.0,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20),
-                    ),
-                    Text(
-                      "Shopping app",
-                      style: TextStyle(
-                          decoration: TextDecoration.none,
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircularProgressIndicator(),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20),
-                  ),
-                  Text(
-                    "Online Store \nfor everyone",
-                    style: TextStyle(
-                        decoration: TextDecoration.none,
-                        color: Colors.white,
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-              flex: 1,
-            )
-          ],
-        )
-      ],
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashPage(),
+        '/verify': (context) => VerifyPage(),
+        '/home': (context) => Home(),
+        '/sign_in': (context) => SignIn(),
+        '/sign_up': (context) => SignUp(),
+      },
+      theme: ThemeData(
+          primaryColor: Colors.purpleAccent, accentColor: Colors.purpleAccent),
+      debugShowCheckedModeBanner: false,
+    ));
---
+++ lib/presentation/screens/verify_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VerifyPage extends StatefulWidget {
+  VerifyPage({Key key}) : super(key: key);
+
+  @override
+  _VerifyPageState createState() => _VerifyPageState();
+}
+
+class _VerifyPageState extends State<VerifyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
